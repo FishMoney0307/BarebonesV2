e64848337137b4d7d138b0dd8806697202a60371
FishMoney0307 <ethan2002@optonline.net>
2024-04-28 18:39:56 -0400

    prelim home page form submission
    doesnt work first try. who woulda guessed

--- frontend/src/Home/Submit.jsx
+++ frontend/src/Home/Submit.jsx
@@ -1,23 +1,96 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams, useNavigate } from "react-router-dom";
 
 const Submit = () => {
+  const [t, setT] = useState('');
+  const [p, setP] = useState('');
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState('typing');
+
+  //MongoDB things
+  const [form, setForm] = useState({
+    title: "",
+    priority: "",
+  });
+  const [isNew, setIsNew] = useState(true);
+  const params = useParams();
+  const navigate = useNavigate(); //might not need this
+
+  async function submit (e) {
+    e.preventDefault();
+    setError(null);
+    setStatus('submitting'); 
+    // /\ I might not need this, but I'll keep it for now if I want to style
+    try {
+      await recordMongoDB (t, p);
+    } catch (err) {
+      setStatus('typing');
+      setError(err);
+    }
+  }
+
+  function titleChange (ev) {
+    setT(ev.target.value);
+  }
+  function sliderChange (ev) {
+    setP(ev.target.value);
+  }
+
   return (
     <div className='flex'>
         <div>
             <h1>Game Entry</h1> <br />
         </div>
         <div>
-            <form id="formed" onsubmit="update()" target="update">
-                <input type="text" id="title" /><br />
+            <form onSubmit={submit}>
+                <input type="text" id="title" value={t} onChange={titleChange} /><br />
 
                 <label for="slider">Priority: </label>
-                <input type='range' id="slider" /><br />
+                <input type='range' id="slider" min="0" max="10" step="1" value={p} onChange={sliderChange} /><br />
 
                 <input type="submit" value="Submit" />
             </form>
         </div>
     </div>
   )
+};
+
+async function fetchData() {
+  const id = params.id?.toString() || undefined;
+  if (!id) return;
+  setIsNew(false);
+  const response = await fetch(
+    `http://localhost:5050/record/${params.id.toString()}`
+  );
+  if (!response.ok) {
+    const message = `An error has occurred: ${response.statusText}`;
+    console.error(message);
+    return;
+  }
+  const record = await response.json();
+  if (!record) {
+    console.warn(`Record with id ${id} not found`);
+    return;
+  }
+}
+
+async function recordMongoDB (t, p) {
+  setForm(t, p, "");
+  const game = { ...form };
+  try {
+    let response;
+    response = await fetch ("http://localhost:5050/record", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(game),
+    });
+  } catch (error) {
+    console.error('A problem occurred adding or updating a record: ', error);
+  } finally {
+    setForm({ title: "", priority: "", level: ""});
+  }
 }
 
 export default Submit
